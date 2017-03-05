f1fb6a2f1d7069478d99e12627595896da0296b8
Tomoyuki Ito <tomoyuki.atago@gmail.com>
2017-03-06 00:19:21 +0900

    add detail page

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/detail', methods=['POST'])
+def detail():
+    return render_template('detail.html', message=request.form['message'])
 
 if __name__ == '__main__':
     app.run()
